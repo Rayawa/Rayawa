4cbbf1de4b327b1a6d6b7f403f083b0c77724ebd
Rayawa <73282963+Rayawa@users.noreply.github.com>
2021-01-26 21:23:25 +0800

    Added starter.exe.cpp

---
+++ mc-pubFiles/Pigeonia Featurehouse Starter.cpp
@@ -0,0 +1,104 @@
+#include<bits/stdc++.h>
+#include<windows.h>
+#include<iostream>
+#include<ctime>
+#include<cmath>
+#include<unistd.h>
+using namespace std;
+void o(string a)
+{
+    for(int i=0;i<a.length();i++)
+    {        
+        cout<<a[i];
+        usleep(8000);
+    }
+    cout<<endl;
+     
+}
+void slow(const string line,int t)
+{
+    for(int i;i<=line.length();i++)
+    {
+        cout<<line[i];
+        usleep(t);
+    }
+}
+int main()
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_GREEN);
+	sleep(2);
+	system("cls");
+	o("Booting...");
+	sleep(1);
+	o("[  OK  ] Rcached target Paths.");
+	o("[  OK  ] Rcached target Basic System.");
+	o("[  OK  ] Found device /github_repo/Featurehouse/sweet_potato-source.");
+	o("[  OK  ] Mounted /git/git-cmd.exe.");
+	o("[  OK  ] Started config.");
+	o("[  OK  ] Rcached target File Systems.");
+	o("[  OK  ] Rcached target Default Target.");
+	o("[  OK  ] Started Plymouth switch root service.");
+	o("[  OK  ] Git configured.");
+	o("         Starting Switch Root...");
+	cout<<endl; 
+	sleep(1);
+	o("Welcome to Pigeonia Featurehouse!");
+	sleep(2);
+
+
+	//cls
+	system("cls");
+	o("Featurehouse Login: Rayawa");
+	o("password: **************");
+	sleep(1);
+	cout<<endl;
+	o("std::output-id = Rayawa");
+	o("std::output-email = Rayawa.work@gamil.com");
+	o("std::output-status = [ ONLINE ] Sweet Potato Mod Author");
+	o("std::output-group = Author");
+	cout<<endl;
+	sleep(1);
+	o("Hi, Rayawa!");
+	o("Last login: 2021-01-10T 11:42:36 +08:00 in Beijing, China");
+	sleep(1);
+	cout<<endl;
+	o("Now Loading files...");
+	sleep(1);
+	o("Enumerating objects: 13, done.");
+	o("Counting objects: 100% (13/13), done.");
+	o("Delta compression using up to 4 threads.");
+	o("Compressing objects: 100% (13/13).");
+	o("Doneuriting obiects: 100% (13/13), 8.16 MiB | 1.5 MiB/s, done.");
+	o("Total 13 (delta 0), reused 0 (delta 0), pack-reused 0");
+	cout<<endl;
+	o("Files Loaded!");
+	sleep(2);
+	
+	//cls
+	system("cls");
+	o("Connecting...");
+	sleep(1);
+	o("Welcome to Pigenia Featurehouse! You are connected to: Featurehouse-Author");
+	cout<<endl;
+	o("Find 6 repositories | 1 web");
+	o("std::output-repository = Featurehouse/sweet_potato-source");
+	o("std::output-repository = Featurehouse/sweet_potato-source-specialRelease");
+	o("std::output-repository = Featurehouse/sweet_potato-release");
+	o("std::output-repository = Featurehouse/sweet_potato-pubfile");
+	o("std::output-repository = Featurehouse/Featurehouse.github.io");
+	o("std::output-repository = Featurehouse/sweet_potato-issueTracker");
+	cout<<endl;
+	o("std::output-web = Featurehouse.github.io");
+	cout<<endl;
+	o("std::output-webs 3:");
+	o("[  OK  ] https://github.com/Featurehouse/sweet_potato-release");
+	o("[  OK  ] https://www.curseforge.com/minecraft/mc-mods/sweet-potato");
+	o("[  OK  ] https://www.mcbbs.net/thread-1132119-1-1.html");
+	cout<<endl<<endl;
+	o("[root@Featurehosue-Author ~]# ./start.sh");
+	sleep(2);
+	system("cls");
+	system("pause");
+	
+	return 0;
+} 
